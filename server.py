6b0238028b0c755931fa7b3793e8b4a96d3da330
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-07-11 15:35:38 -0700

    updates cleaning up Jquery code and simplifying SQL queries.

--- server.py
+++ server.py
@@ -11,68 +11,31 @@ def main():
 
 @app.route('/leads', methods = ['GET', 'POST'])
 def leadLinks():
-	query1 = "SELECT count(id) AS numLeads FROM leads"
-	query2 = "SELECT count(id) AS numLeads FROM leads WHERE (first_name LIKE concat('%',:search,'%') OR last_name LIKE concat('%',:search,'%')) AND (registered_datetime BETWEEN :dateFrom AND :dateTo)"
-	query3 = "SELECT registered_datetime FROM leads ORDER BY registered_datetime LIMIT 1"
-	dateFrom = mysql.query_db(query3)
-	dateTo = datetime.datetime.now().strftime('%Y-%m-%d')
-	if request.method == 'POST':
-		if len(request.form['dateFrom']) == 0:
-			dFrom = dateFrom[0]['registered_datetime'].strftime('%Y-%m-%d')
-		else:
-			dFrom = request.form['dateFrom']
-		if len(request.form['dateTo']) == 0:
-			dTo = dateTo
-		else:
-			dTo = request.form['dateTo']
-		info2 = {
-			'search': request.form['name'],
-			'dateFrom': dFrom,
-			'dateTo': dTo
-		}
-		numLeads = mysql.query_db(query2, info2);
-	else:
-		numLeads = mysql.query_db(query1);
-	numLeads = float(numLeads[0]['numLeads']);
-	numLeads = math.ceil(numLeads/5);
-	numLeads = int(numLeads);
+	query2 = "SELECT count(id) AS numLeads FROM leads WHERE (first_name LIKE concat('%',:search,'%') OR last_name LIKE concat('%',:search,'%')) AND (registered_datetime > :dateFrom AND registered_datetime < :dateTo)"
+	info2 = {
+		'search': request.form['name'],
+		'dateFrom': request.form['dateFrom']+'%',
+		'dateTo': request.form['dateTo']+ 'null'
+	}
+	numLeads = mysql.query_db(query2, info2);
+	numLeads = math.ceil(float(numLeads[0]['numLeads'])/5);
 	links = [];
 	num = 0;
-	for x in xrange(0,numLeads):
+	for x in xrange(0,int(numLeads)):
 		links.append("/leads/show/"+str(num))
 		num += 5
 	return render_template('links.html', links = links)
 
-
 @app.route('/leads/show/<num>', methods = ['GET','POST'])
 def showLeads(num):
-	query1 = "SELECT DISTINCT * FROM leads WHERE (first_name LIKE concat('%',:search,'%') OR last_name LIKE concat('%',:search,'%')) AND (registered_datetime BETWEEN :dFrom AND :dTo) LIMIT :num, 5"
-	query2 = "SELECT * FROM leads LIMIT :num, 5"
-	query3 = "SELECT registered_datetime FROM leads ORDER BY registered_datetime LIMIT 1"
-	dateFrom = mysql.query_db(query3)
-	dateTo = datetime.datetime.now().strftime('%Y-%m-%d')
-	if request.method == 'POST':
-		if len(request.form['dateFrom']) == 0:
-			dFrom = dateFrom[0]['registered_datetime'].strftime('%Y-%m-%d')
-		else:
-			dFrom = request.form['dateFrom']
-		if len(request.form['dateTo']) == 0:
-			dTo = dateTo
-		else:
-			dTo = request.form['dateTo']
-		info1 = {
-			'search': request.form['name'],
-			'dFrom': dFrom,
-			'dTo': dTo,
-			'num': int(num)
-		}
-		leads = mysql.query_db(query1, info1)
-	else:
-		info2 = {
-			'num': int(num)
-		}
-		leads = mysql.query_db(query2, info2)
+	query1 = "SELECT * FROM leads WHERE (first_name LIKE concat('%',:search,'%') OR last_name LIKE concat('%',:search,'%')) AND (registered_datetime > :dateFrom AND registered_datetime < :dateTo) LIMIT :num, 5"
+	info1 = {
+		'search': request.form['name'],
+		'dateFrom': request.form['dateFrom']+'%',
+		'dateTo': request.form['dateTo']+'null',
+		'num': int(num)
+	}
+	leads = mysql.query_db(query1, info1)
 	return render_template('table.html', leads = leads)
-
 
 app.run(debug=True)
